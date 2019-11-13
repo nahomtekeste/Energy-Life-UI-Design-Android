19e71bc285a04018bc050a53f35aa62cddd0ddd6
nahomtekeste <46280184+nahomtekeste@users.noreply.github.com>
2019-11-13 19:12:56 +0000

    Initial commit

---
+++ app/src/main/java/com/example/energy_life_ui_design_android/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.energy_life_ui_design_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
---
+++ app/src/main/java/com/example/energy_life_ui_design_android/MoreInfo.kt
@@ -0,0 +1,11 @@
+package com.example.energy_life_ui_design_android
+
+import android.os.Bundle
+
+class MoreInfo : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
